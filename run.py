64720bb06bb8c258a85815d84616471428420c53
Guillaume ROZIER <guillaume.rozier@telecomnancy.net>
2020-03-15 17:53:59 +0100

    updates to run.py

--- run.py
+++ run.py
@@ -5,9 +5,20 @@ import subprocess
 k=0
 
 while True is True:
-        subprocess.run(["git", "pull"])
-	subprocess.run(["python3", "covid-19.py"])
-	print(">>>>> " + datetime.now())	
+    subprocess.run(["git", "pull"])
+
+    if k%4 == 0: # do upload to plotly
+        subprocess.run(["python3", "covid-19.py", "True", "False"])
+
+    else: #do not upload to plotly
+        subprocess.run(["python3", "covid-19.py", "False", "False"])
+
+    subprocess.run(["git", "add", "images/*"])
+    subprocess.run(["git", "commit", "-m", "Charts updated (image folder)"])
+    subprocess.run(["git", "push"])
+
+	print(">>>>> " + datetime.now())
 	print("---- Iteration " + k)
 	k += 1
-	time.sleep(14400)
+
+	time.sleep(3600)
