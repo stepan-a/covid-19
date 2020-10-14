eaa9b9cd46d1852adc6a4051b26a445b8aff2d86
Guillaume ROZIER <guillaume.rozier@telecomnancy.net>
2020-10-14 18:00:36 +0200

    update script

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ while [ true ];
 do
 	sleep 5
 	echo start_script
-	sudo git fetch --all && sudo git reset --hard origin/master && sudo jupyter  nbconvert --to script *.ipynb && sudo python3 script_update_data.py > /home/pi/Documents/covid-19/cronlogs 2>&1
+	sudo git fetch --all && sudo git reset --hard origin/master && sudo jupyter  nbconvert --to script *.ipynb && sudo python3 script_update_data.py
 	sleep 60
 done
